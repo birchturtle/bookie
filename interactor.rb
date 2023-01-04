f78c723bc7c0bb1c7e5709baf0d477658ce79c3d
birchturtle <kj@birchturtle.net>
2023-01-04 12:06:33 +0100

    put general 'interface' in Main; Call new Interactor class for actions on entities

---
+++ interactor.rb
@@ -0,0 +1,10 @@
+require("./book.rb")
+require("./db.rb")
+
+# frozen_string_literal: true
+
+class Interactor
+  def add_author
+    puts "Adding author!"
+  end
+end
--- main.rb
+++ main.rb
@@ -1,12 +1,57 @@
-require("./book.rb")
-require("./db.rb")
-
+require './interactor.rb'
 class Main
+  INTERACTOR = Interactor.new
   def run
-    a = Author.new("Kurt")
-    g = Genre.new("Horror", "Skøn")
-    b = Book.new("Bogens titel", false, a, g, 2)
-    b2 = Book.new("Anden bogs titel", true, a, g, 1)
+    if ARGV[0] == '-h' then
+      print_help
+      exit 0
+    end
+    init_menu
+  end
+
+  private
+  def print_help
+    puts "Useful help text here :-)"
+  end
+  def init_menu
+    puts "Do?"
+    puts <<-MENU
+      1) List
+      2) Delete
+      3) Add
+    MENU
+    action = gets
+    case action.tr("\n", "")
+             when "1"
+               entity_menu "list"
+             when "2"
+               entity_menu "delete"
+             when "3"
+               entity_menu "add"
+             else
+               print_help
+               exit 0
+             end
+  end
+  def entity_menu(action)
+    puts "Do?"
+    puts <<-MENU
+      1) Book
+      2) Author
+      3) Genre
+    MENU
+    entity = gets
+    case entity.tr("\n", "")
+             when "1"
+               INTERACTOR.send((action + "_" + "book").to_sym)
+             when "2"
+               INTERACTOR.send((action + "_" + "author").to_sym)
+             when "3"
+               INTERACTOR.send((action + "_" + "genre").to_sym)
+             else
+               print_help
+               exit 0
+             end
   end
 end
 
